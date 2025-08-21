d42d29e7bfb4d1ae43423cf08f18e97e36eabe9d
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-08-22 01:17:37 +0200

    Added new method to get all customer's products

--- Customer-Service/src/main/java/com/yann/customerservice/application/CustomerService.java
+++ Customer-Service/src/main/java/com/yann/customerservice/application/CustomerService.java
@@ -3,6 +3,7 @@ package com.yann.customerservice.application;
 import com.yann.customerservice.application.dto.CustomerProductRequestDTO;
 import com.yann.customerservice.application.dto.CustomerRequestDTO;
 import com.yann.customerservice.application.dto.CustomerResponseDTO;
+import com.yann.customerservice.application.dto.ProductCustomerResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,4 +18,6 @@ public interface CustomerService {
 
     CustomerResponseDTO addProductToCustomer(
             String customerId, CustomerProductRequestDTO customerProductRequestDTO);
+
+    List<ProductCustomerResponseDTO> getCustomersProductsList(String customerId);
 }
