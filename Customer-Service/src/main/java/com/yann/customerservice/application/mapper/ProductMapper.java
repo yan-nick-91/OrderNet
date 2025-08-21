3c83a8e59ae87fd41cbe054c2dedde5485429d3b
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-08-22 01:16:44 +0200

    Added new mapper method

--- Customer-Service/src/main/java/com/yann/customerservice/application/mapper/ProductMapper.java
+++ Customer-Service/src/main/java/com/yann/customerservice/application/mapper/ProductMapper.java
@@ -4,9 +4,13 @@ import com.yann.customerservice.application.dto.ProductCustomerResponseDTO;
 import com.yann.customerservice.domain.Product;
 
 public class ProductMapper {
-    public static Product toProduct(ProductCustomerResponseDTO productCustomerResponseDTO ) {
+    public static Product toProduct(ProductCustomerResponseDTO productCustomerResponseDTO) {
         return new Product(productCustomerResponseDTO.productID(),
                 productCustomerResponseDTO.name(),
                 productCustomerResponseDTO.price());
+    }
+
+    public static ProductCustomerResponseDTO toProductCustomerResponseDTO(Product product) {
+        return new ProductCustomerResponseDTO(product.getId(), product.getProductName(), product.getPrice());
     }
 }
