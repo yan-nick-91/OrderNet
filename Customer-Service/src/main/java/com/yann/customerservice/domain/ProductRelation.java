8db8da1f01c9045625f290b2b3af21f91126f4ce
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-08-25 23:26:52 +0200

    Added quantity in ProductRelation

--- Customer-Service/src/main/java/com/yann/customerservice/domain/ProductRelation.java
+++ Customer-Service/src/main/java/com/yann/customerservice/domain/ProductRelation.java
@@ -1,5 +1,6 @@
 package com.yann.customerservice.domain;
 
+import com.yann.customerservice.domain.exceptions.IllegalProductQuantityException;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
@@ -12,13 +13,18 @@ public class ProductRelation {
     private Long id;
 
     private final ProductRelationType productRelationType;
+    private int quantity;
 
     @TargetNode
     private final Product product;
 
-    public ProductRelation(Product product, ProductRelationType productRelationType) {
+    public ProductRelation(Product product, ProductRelationType productRelationType, int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalProductQuantityException("Quantity must be greater than 0");
+        }
         this.product = product;
         this.productRelationType = productRelationType;
+        this.quantity = quantity;
     }
 
     public ProductRelationType getProductRelationType() {
@@ -27,5 +33,23 @@ public class ProductRelation {
 
     public Product getProduct() {
         return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void increaseQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalProductQuantityException("Quantity must be greater than 0");
+        }
+        this.quantity += quantity;
+    }
+
+    public void decreaseQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalProductQuantityException("Quantity must be greater than 0");
+        }
+        this.quantity -= quantity;
     }
 }
