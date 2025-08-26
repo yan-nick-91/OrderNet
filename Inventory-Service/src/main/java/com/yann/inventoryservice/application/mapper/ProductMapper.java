54f1b160cac6f55f76c96f2b3311860b1f5361a9
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-08-26 12:59:09 +0200

    Added mapper methods for response dto

--- Inventory-Service/src/main/java/com/yann/inventoryservice/application/mapper/ProductMapper.java
+++ Inventory-Service/src/main/java/com/yann/inventoryservice/application/mapper/ProductMapper.java
@@ -1,10 +1,8 @@
 package com.yann.inventoryservice.application.mapper;
 
-import com.yann.inventoryservice.application.dto.ProductCustomerResponseDTO;
-import com.yann.inventoryservice.application.dto.ProductRequestDTO;
-import com.yann.inventoryservice.application.dto.ProductResponseDTO;
-import com.yann.inventoryservice.application.dto.StockResponseDTO;
+import com.yann.inventoryservice.application.dto.*;
 import com.yann.inventoryservice.domain.Product;
+import com.yann.inventoryservice.domain.vo.MaxQuantity;
 import com.yann.inventoryservice.domain.vo.ProductID;
 
 import java.time.LocalDate;
@@ -17,7 +15,13 @@ public class ProductMapper {
     public static Product toProduct(ProductRequestDTO productRequestDTO) {
         ProductID productID = new ProductID(String.format("PROD-ID-%s-%s", dateFormat, UUID.randomUUID()));
         return new Product(productID, productRequestDTO.name(), productRequestDTO.price(),
-                productRequestDTO.quantity(), productRequestDTO.maxQuantity());
+                productRequestDTO.quantity(), new MaxQuantity(productRequestDTO.maxQuantity()));
+    }
+
+    public static Product toUpdateProduct(Product product, StockUpdateRequestDTO stockUpdateRequestDTO) {
+        product.setName(stockUpdateRequestDTO.name());
+        product.setPrice(stockUpdateRequestDTO.price());
+        return product;
     }
 
     public static ProductResponseDTO toProductRequestDTO(Product product) {
@@ -25,12 +29,17 @@ public class ProductMapper {
                 product.getPrice(), product.getAvailableQuantity(), product.getMaxQuantity());
     }
 
-    public static ProductCustomerResponseDTO productCustomerResponseDTO(Product product) {
+    public static ProductCustomerResponseDTO toProductCustomerResponseDTO(Product product) {
         return new ProductCustomerResponseDTO(product.getId(), product.getName(), product.getPrice());
     }
 
     public static StockResponseDTO toStockResponseDTO(Product product) {
         return new StockResponseDTO(product.getId(), product.getName(),
                 product.getAvailableQuantity(), product.checkAvailability());
+    }
+
+    public static StockUpdateResponseDTO toStockUpdateResponse(Product product) {
+        return new StockUpdateResponseDTO(product.getId(), product.getName(),
+                product.getPrice(), product.getMaxQuantity());
     }
 }
