63f23c2d5f7a516861462fb8ad32038761d5af60
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-08-29 16:32:44 +0200

    Updated or added new methods:
    - Method to adjust the quantity of product is implemented
    
    - Method to delete customer is implemented
    
    - Refactored method findProductsByCustomerID

--- Customer-Service/src/main/java/com/yann/customerservice/presentation/CustomerController.java
+++ Customer-Service/src/main/java/com/yann/customerservice/presentation/CustomerController.java
@@ -1,11 +1,10 @@
 package com.yann.customerservice.presentation;
 
 import com.yann.customerservice.application.CustomerService;
+import com.yann.customerservice.application.dto.AdjustProductQuantityRequestDTO;
 import com.yann.customerservice.application.dto.CustomerProductRequestDTO;
 import com.yann.customerservice.application.dto.CustomerRequestDTO;
-import com.yann.customerservice.domain.exceptions.CustomerAlreadyExistsException;
-import com.yann.customerservice.domain.exceptions.CustomerNotFoundException;
-import com.yann.customerservice.domain.exceptions.ProductUnavailableException;
+import com.yann.customerservice.domain.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,7 +36,9 @@ public class CustomerController {
             @PathVariable String customerIDAsString, @RequestBody CustomerProductRequestDTO customerProductRequestDTO) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(
-                    customerService.addProductToCustomer(customerIDAsString, customerProductRequestDTO));
+                    customerService.initializeProductToCart(customerIDAsString, customerProductRequestDTO));
+        } catch (ProductAlreadyInitializedInCartException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (CustomerNotFoundException | HttpClientErrorException.NotFound e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (ProductUnavailableException e) {
@@ -72,7 +73,38 @@ public class CustomerController {
     @GetMapping("/{customerIDAsString}/products")
     public ResponseEntity<Object> findProductsByCustomerID(@PathVariable String customerIDAsString) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomersProductsList(customerIDAsString));
+            return ResponseEntity.status(HttpStatus.OK)
+                                 .body(customerService.getCustomersProductsList(customerIDAsString));
+        } catch (CustomerNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{customerIDAsString}/products/quantity")
+    public ResponseEntity<Object> updateProductQuantity(
+            @PathVariable String customerIDAsString,
+            @RequestBody AdjustProductQuantityRequestDTO adjustProductQuantityRequestDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                                 .body(customerService.adjustQuantityOfExistingProductInCart(
+                                         customerIDAsString, adjustProductQuantityRequestDTO));
+        } catch (IllegalProductQuantityException | IllegalAdjustmentTypeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (CustomerNotFoundException | ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{customerIDAsString}")
+    public ResponseEntity<Object> deleteCustomer(@PathVariable String customerIDAsString) {
+        try {
+            customerService.deleteCustomer(customerIDAsString);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                                 .body("Customer " + customerIDAsString + " deleted successfully");
         } catch (CustomerNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
