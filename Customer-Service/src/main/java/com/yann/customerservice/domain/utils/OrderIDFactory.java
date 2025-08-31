154be7c4b13d898b397470bbd536ddd158073b99
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-01 00:24:56 +0200

    Init OrderIDFactory

---
+++ Customer-Service/src/main/java/com/yann/customerservice/domain/utils/OrderIDFactory.java
@@ -0,0 +1,22 @@
+package com.yann.customerservice.domain.utils;
+
+import com.yann.customerservice.domain.vo.OrderID;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Component
+class OrderIDFactory implements CreateIDFactory<OrderID> {
+    @Override
+    public OrderID create() {
+        String dateFormat = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        return new OrderID(String.format("ORD-ID-%s-%s", dateFormat, UUID.randomUUID()));
+    }
+
+    @Override
+    public OrderID set(String id) {
+        return new OrderID(id);
+    }
+}
