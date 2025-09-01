f64819b9f2432ff6332c624d4d0847c98f9d3acb
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-02 01:41:01 +0200

    Added get methods to for persisted orders

--- Orders-Service/src/main/java/com/yann/ordersservice/application/services/OrderServiceImpl.java
+++ Orders-Service/src/main/java/com/yann/ordersservice/application/services/OrderServiceImpl.java
@@ -1,12 +1,17 @@
 package com.yann.ordersservice.application.services;
 
+import com.yann.ordersservice.application.dto.OrdersResponseDTO;
 import com.yann.ordersservice.application.dto.PaymentResponseDTO;
 import com.yann.ordersservice.application.mapper.OrderMapper;
 import com.yann.ordersservice.domain.Order;
+import com.yann.ordersservice.domain.Product;
+import com.yann.ordersservice.domain.exceptions.OrderNotFound;
 import com.yann.ordersservice.domain.utils.CreateIDFactory;
 import com.yann.ordersservice.domain.vo.OrderID;
 import com.yann.ordersservice.infrastructure.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 class OrderServiceImpl implements OrderService {
@@ -20,8 +25,22 @@ class OrderServiceImpl implements OrderService {
 
     @Override
     public void saveIncomingOrderFromCustomer(PaymentResponseDTO paymentResponseDTO) {
-            OrderID orderID = orderIDFactory.set(paymentResponseDTO.orderID());
-            Order order = OrderMapper.toOrder(orderID, paymentResponseDTO);
-            orderRepository.save(order);
+        OrderID orderID = orderIDFactory.set(paymentResponseDTO.orderID());
+        Order order = OrderMapper.toOrder(orderID, paymentResponseDTO);
+        orderRepository.save(order);
+    }
+
+    @Override
+    public List<OrdersResponseDTO> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream().map(OrderMapper::toOrdersResponseDTO).toList();
+    }
+
+    @Override
+    public OrdersResponseDTO getOrderById(String orderIDAsString) {
+        OrderID orderID = orderIDFactory.set(orderIDAsString);
+        Order order = orderRepository.findById(orderID)
+                                     .orElseThrow(() -> new OrderNotFound("Order not found"));
+        return OrderMapper.toOrdersResponseDTO(order);
     }
 }
