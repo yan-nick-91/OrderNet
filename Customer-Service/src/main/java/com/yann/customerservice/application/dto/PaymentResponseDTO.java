133b1e2c7598150fc4c19945d4ee22d3cccf8fd0
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-01 21:29:03 +0200

    Refactoring code styling

--- Customer-Service/src/main/java/com/yann/customerservice/application/dto/PaymentResponseDTO.java
+++ Customer-Service/src/main/java/com/yann/customerservice/application/dto/PaymentResponseDTO.java
@@ -3,5 +3,6 @@ package com.yann.customerservice.application.dto;
 public record PaymentResponseDTO(
         String orderID,
         String orderDate,
-        CustomerResponseDTO customer) {
+        CustomerResponseDTO customer
+) {
 }
