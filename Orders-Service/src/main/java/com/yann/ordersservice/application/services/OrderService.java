fe3e0031be7a5147237a9d1be3862a2d411a9a55
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-01 21:33:42 +0200

    Updated OrderService

--- Orders-Service/src/main/java/com/yann/ordersservice/application/services/OrderService.java
+++ Orders-Service/src/main/java/com/yann/ordersservice/application/services/OrderService.java
@@ -1,4 +1,7 @@
 package com.yann.ordersservice.application.services;
 
+import com.yann.ordersservice.application.dto.PaymentResponseDTO;
+
 public interface OrderService {
+    void saveIncomingOrderFromCustomer(PaymentResponseDTO paymentResponseDTO);
 }
