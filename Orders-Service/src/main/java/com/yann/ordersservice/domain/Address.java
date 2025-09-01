c3d48f83d72650096f56059292befba7fd1ffe6e
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-01 17:49:26 +0200

    Init/Updated Address Class for orders-service

--- Orders-Service/src/main/java/com/yann/ordersservice/domain/Address.java
+++ Orders-Service/src/main/java/com/yann/ordersservice/domain/Address.java
@@ -1,18 +1,31 @@
 package com.yann.ordersservice.domain;
 
 import com.yann.ordersservice.domain.vo.StreetNumber;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.neo4j.core.schema.Id;
 
+@Document("Address")
 public class Address {
+    @Id
+    private String zipcode;
     private String streetName;
     private StreetNumber streetNumber;
     private String city;
     private String country;
 
-    public Address(String streetName, StreetNumber streetNumber, String city, String country) {
+    public Address() {
+    }
+
+    public Address(String zipcode, String streetName, StreetNumber streetNumber, String city, String country) {
+        this.zipcode = zipcode;
         this.streetName = streetName;
         this.streetNumber = streetNumber;
         this.city = city;
         this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
     }
 
     public String getStreetName() {
