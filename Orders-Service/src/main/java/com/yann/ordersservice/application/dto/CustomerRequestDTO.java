d4264fa8778a168c99921cbcce8eb8f02d5c8ca4
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-01 21:39:53 +0200

    Removed DTO

--- Orders-Service/src/main/java/com/yann/ordersservice/application/dto/CustomerRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.yann.ordersservice.application.dto;
-
-public record CustomerRequestDTO(
-        String firstName,
-        String lastName,
-        String email,
-        AddressDTO address
-) {
-}
