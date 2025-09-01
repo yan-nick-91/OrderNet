2d111a444bb8868cf221a988f9c726bf6af3f7aa
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-01 21:22:35 +0200

    Refactoring code styling

--- Customer-Service/src/main/java/com/yann/customerservice/application/dto/CustomerProductRequestDTO.java
+++ Customer-Service/src/main/java/com/yann/customerservice/application/dto/CustomerProductRequestDTO.java
@@ -1,4 +1,7 @@
 package com.yann.customerservice.application.dto;
 
-public record CustomerProductRequestDTO(String name, int quantity) {
+public record CustomerProductRequestDTO(
+        String name,
+        int quantity
+) {
 }
--- Customer-Service/src/main/java/com/yann/customerservice/application/dto/ProductCustomerResponseDTO.java
+++ Customer-Service/src/main/java/com/yann/customerservice/application/dto/ProductCustomerResponseDTO.java
@@ -2,5 +2,9 @@ package com.yann.customerservice.application.dto;
 
 import com.yann.customerservice.domain.vo.ProductID;
 
-public record ProductCustomerResponseDTO(ProductID productID, String name, double price) {
+public record ProductCustomerResponseDTO(
+        ProductID productID,
+        String name,
+        double price
+) {
 }
