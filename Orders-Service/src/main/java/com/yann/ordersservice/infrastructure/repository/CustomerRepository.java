d250c331fc995bfb12a040a6bfa08081e73668cc
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-05 16:47:02 +0200

    Removed CustomerRepository

--- Orders-Service/src/main/java/com/yann/ordersservice/infrastructure/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yann.ordersservice.infrastructure.repository;
-
-import com.yann.ordersservice.domain.Customer;
-import com.yann.ordersservice.domain.vo.CustomerID;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, CustomerID> {
-}
