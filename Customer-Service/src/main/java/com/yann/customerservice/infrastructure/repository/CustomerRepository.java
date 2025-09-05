cd16b749671b67f7bcc709c969d2dbfb4009c392
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-05 16:46:00 +0200

    Added method to find customer by email

--- Customer-Service/src/main/java/com/yann/customerservice/infrastructure/repository/CustomerRepository.java
+++ Customer-Service/src/main/java/com/yann/customerservice/infrastructure/repository/CustomerRepository.java
@@ -2,7 +2,11 @@ package com.yann.customerservice.infrastructure.repository;
 
 import com.yann.customerservice.domain.Customer;
 import com.yann.customerservice.domain.vo.CustomerID;
+import com.yann.customerservice.domain.vo.Email;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends Neo4jRepository<Customer, CustomerID> {
+    Optional<Customer> findByEmail(Email email);
 }
