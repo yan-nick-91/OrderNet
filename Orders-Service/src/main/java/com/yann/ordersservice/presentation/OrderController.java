24c3a9dac958355c5c08e623e23602357fea9739
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-22 23:03:02 +0200

    Added method sendOrderToInventory

--- Orders-Service/src/main/java/com/yann/ordersservice/presentation/OrderController.java
+++ Orders-Service/src/main/java/com/yann/ordersservice/presentation/OrderController.java
@@ -4,10 +4,7 @@ import com.yann.ordersservice.application.services.OrderService;
 import com.yann.ordersservice.domain.exceptions.OrderNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
@@ -45,7 +42,6 @@ public class OrderController {
         }
     }
 
-
     @GetMapping("/{orderIDAsString}")
     public ResponseEntity<Object> getOrder(@PathVariable String orderIDAsString) {
         try {
@@ -57,4 +53,14 @@ public class OrderController {
         }
     }
 
+    @PostMapping("/{orderIDAsString}")
+    public ResponseEntity<Object> sendOrderToInventory(@PathVariable String orderIDAsString) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(orderService.sendOrderToInventory(orderIDAsString));
+        } catch (OrderNotFound e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
 }
