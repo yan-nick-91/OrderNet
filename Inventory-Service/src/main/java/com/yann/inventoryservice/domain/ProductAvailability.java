9e6217f7f564c4acdf2237ebccab6be8972b5e33
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-09-22 23:07:25 +0200

    Updated Class ProductAvailability
    - Added new method
    
    - Renamed names of method

--- Inventory-Service/src/main/java/com/yann/inventoryservice/domain/ProductAvailability.java
+++ Inventory-Service/src/main/java/com/yann/inventoryservice/domain/ProductAvailability.java
@@ -1,7 +1,8 @@
 package com.yann.inventoryservice.domain;
 
-import com.yann.inventoryservice.domain.exception.IllegaInitInventoryException;
+import com.yann.inventoryservice.domain.exception.IllegalInitInventoryException;
 import com.yann.inventoryservice.domain.exception.IllegalInventoryUpdateException;
+import com.yann.inventoryservice.domain.exception.IllegalProductAvailabilityException;
 import com.yann.inventoryservice.domain.exception.OutOfStockException;
 import org.springframework.stereotype.Component;
 
@@ -9,35 +10,47 @@ import java.util.List;
 
 @Component
 public class ProductAvailability {
-    public String checkAvailability(Product product) {
-        if (product.getAvailableQuantity() <= 0) {
-            throw new OutOfStockException("Product is out of stock. Please refill");
-        }
+    public String displayAvailability(Product product) {
+        validateProductStock(product);
+        double percentage = calculateAvailabilityPercentage(product);
 
-        double percentage = getAvailabilityPercentage(product);
-
-        if (percentage <= 50) {
+        if (percentage <= 50.0) {
             return String.format("Product is low: %.2f%%.", percentage);
         }
         return String.format("Product in stock: %.2f%%", percentage);
     }
 
-    private double getAvailabilityPercentage(Product product) {
-        if (product.getMaxQuantity().value() < 50) {
-            throw new IllegaInitInventoryException("The initialization of a product cannot be lower than 50");
-        }
-        return ((double) product.getAvailableQuantity() / product.getMaxQuantity().value()) * 100;
-    }
-
-    public void checkIfProductIsAlreadyInitialized(Product newProduct, List<Product> products) {
+    public void verifyIfProductIsInStock(Product newProduct, List<Product> products) {
         boolean productNameExists = products.stream()
                                             .map(Product::getName)
-                                            .anyMatch(existingName -> existingName.equals(newProduct.getName()));
+                                            .anyMatch(newProduct.getName()::equals);
 
         if (productNameExists) {
             throw new IllegalInventoryUpdateException(
                     "Cannot add product: '" + newProduct.getName() + "' is already in inventory."
             );
         }
+    }
+
+    // Helpers
+    private void validateProductStock(Product product) {
+        int availableQuantity = product.getAvailableQuantity();
+        int maxQuantity = product.getMaxQuantity().value();
+
+        if (maxQuantity < 50) {
+            throw new IllegalInitInventoryException("The initialization of a product cannot be lower than 50");
+        }
+
+        if (availableQuantity < 0) {
+            throw new IllegalProductAvailabilityException("The available quantity cannot be less than 0");
+        }
+
+        if (availableQuantity == 0) {
+            throw new OutOfStockException("The available quantity cannot be zero");
+        }
+    }
+
+    private double calculateAvailabilityPercentage(Product product) {
+        return ((double) product.getAvailableQuantity() / product.getMaxQuantity().value()) * 100;
     }
 }
