2b2bc27e519a6a89b98c9ec7b358908541c131a5
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-02 19:09:16 +0200

    Added new method to test the value method in VO

--- Inventory-Service/src/test/java/com/yann/inventoryservice/domain/vo/OrderIDTest.java
+++ Inventory-Service/src/test/java/com/yann/inventoryservice/domain/vo/OrderIDTest.java
@@ -3,6 +3,10 @@ package com.yann.inventoryservice.domain.vo;
 import com.yann.inventoryservice.domain.utils.IDFactory;
 import com.yann.inventoryservice.domain.utils.OrderIDFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderIDTest extends AbstractValueObjectTest<OrderID> {
     private IDFactory<OrderID> orderIDFactory;
@@ -25,5 +29,14 @@ class OrderIDTest extends AbstractValueObjectTest<OrderID> {
     @Override
     protected OrderID differentValidInstance() {
         return orderIDFactory.set("ORD-ID-20250926-123e5567-e89b-12d3-a456-426614174223");
+    }
+
+    @Test
+    @DisplayName("Verify If Value Method Equals Instantiated Constructor Value")
+    void verifyIfValueMethodEqualsInstantiatedConstructorValue() {
+        String stringIDValue = "ORD-ID-20250926-123e5567-e89b-12d3-a456-426614174223";
+        OrderID orderID = orderIDFactory.set(stringIDValue);
+
+        assertEquals(stringIDValue, orderID.value());
     }
 }
--- Inventory-Service/src/test/java/com/yann/inventoryservice/domain/vo/ProductIDTest.java
+++ Inventory-Service/src/test/java/com/yann/inventoryservice/domain/vo/ProductIDTest.java
@@ -3,6 +3,10 @@ package com.yann.inventoryservice.domain.vo;
 import com.yann.inventoryservice.domain.utils.IDFactory;
 import com.yann.inventoryservice.domain.utils.ProductIDFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class ProductIDTest extends AbstractValueObjectTest<ProductID>{
@@ -26,5 +30,14 @@ class ProductIDTest extends AbstractValueObjectTest<ProductID>{
     @Override
     protected ProductID differentValidInstance() {
         return productIDFactory.set("PROD-ID-20250926-123e4569-e89b-12d3-a456-426614174001");
+    }
+
+    @Test
+    @DisplayName("Verify If Value Method Equals Instantiated Constructor Value")
+    void verifyIfValueMethodEqualsInstantiatedConstructorValue() {
+        String stringIDValue = "PROD-ID-20250926-123e5567-e89b-12d3-a456-426614174001";
+        ProductID productID = productIDFactory.set(stringIDValue);
+
+        assertEquals(stringIDValue, productID.value());
     }
 }
