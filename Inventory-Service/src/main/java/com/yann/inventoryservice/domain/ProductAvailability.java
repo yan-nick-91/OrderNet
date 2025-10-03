7da38d872dd598a461b34326a67dbbc0c2d4ca72
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-03 22:41:47 +0200

    Taken out if condition

--- Inventory-Service/src/main/java/com/yann/inventoryservice/domain/ProductAvailability.java
+++ Inventory-Service/src/main/java/com/yann/inventoryservice/domain/ProductAvailability.java
@@ -3,7 +3,6 @@ package com.yann.inventoryservice.domain;
 import com.yann.inventoryservice.domain.exception.IllegalInitInventoryException;
 import com.yann.inventoryservice.domain.exception.IllegalInventoryUpdateException;
 import com.yann.inventoryservice.domain.exception.IllegalProductAvailabilityException;
-import com.yann.inventoryservice.domain.exception.OutOfStockException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -43,10 +42,6 @@ public class ProductAvailability {
 
         if (availableQuantity < 0) {
             throw new IllegalProductAvailabilityException("The available quantity cannot be less than 0");
-        }
-
-        if (availableQuantity == 0) {
-            throw new OutOfStockException("The available quantity cannot be zero");
         }
     }
 
