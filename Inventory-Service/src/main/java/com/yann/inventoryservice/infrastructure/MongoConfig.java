1af1879f522ab2d9fd643b0dc907e6260a17286f
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-03 22:32:42 +0200

    Set annotation to ignore the tests

--- Inventory-Service/src/main/java/com/yann/inventoryservice/infrastructure/MongoConfig.java
+++ Inventory-Service/src/main/java/com/yann/inventoryservice/infrastructure/MongoConfig.java
@@ -5,12 +5,14 @@ import com.mongodb.client.MongoClients;
 import com.yann.inventoryservice.infrastructure.converters.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 import java.util.List;
 
 @Configuration
+@Profile("!test")
 public class MongoConfig extends AbstractMongoClientConfiguration {
     @Value("${spring.data.mongodb.database}")
     private String database;
