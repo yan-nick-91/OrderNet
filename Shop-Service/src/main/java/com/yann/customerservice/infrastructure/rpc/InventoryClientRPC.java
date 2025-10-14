e8188dfa0c9cac68072259ca65bee11a43abb3e7
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-14 22:11:10 +0200

    Removed Service annotation

--- Shop-Service/src/main/java/com/yann/customerservice/infrastructure/rpc/InventoryClientRPC.java
+++ Shop-Service/src/main/java/com/yann/customerservice/infrastructure/rpc/InventoryClientRPC.java
@@ -1,11 +1,9 @@
 package com.yann.customerservice.infrastructure.rpc;
 
 import com.yann.customerservice.application.dto.ProductCustomerResponseDTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface InventoryClientRPC {
     ProductCustomerResponseDTO requestProduct(String productName);
     List<ProductCustomerResponseDTO> requestAllProducts();
