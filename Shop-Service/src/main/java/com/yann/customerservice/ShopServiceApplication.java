a30f0c23c21340b34b376d1b27864d620a0397e5
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-15 16:51:44 +0200

    Added EnableScheduling annotation

--- Shop-Service/src/main/java/com/yann/customerservice/ShopServiceApplication.java
+++ Shop-Service/src/main/java/com/yann/customerservice/ShopServiceApplication.java
@@ -2,8 +2,10 @@ package com.yann.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShopServiceApplication {
 
     public static void main(String[] args) {
