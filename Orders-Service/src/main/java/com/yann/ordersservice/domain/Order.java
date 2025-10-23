d090e474a8ebaefced5950ef40afabb0321f65df
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-23 23:56:14 +0200

    Removed method parseDate

--- Orders-Service/src/main/java/com/yann/ordersservice/domain/Order.java
+++ Orders-Service/src/main/java/com/yann/ordersservice/domain/Order.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
-import java.time.format.DateTimeParseException;
 
 @Document(collection = "orders")
 public class Order {
@@ -15,26 +14,17 @@ public class Order {
     private Sequence sequence;
     private Instant date;
     private OrderState state;
-
     private Customer customer;
 
     public Order() {
     }
 
-    public Order(OrderID orderID, String orderDateAsString, Customer customer) {
+    public Order(OrderID orderID, Customer customer) {
         this.orderID = orderID;
         this.sequence = new Sequence();
-        this.date = parseDate(orderDateAsString);
+        this.date = Instant.now();
         this.customer = customer;
         this.state = orderStateSetToPaid(customer);
-    }
-
-    private Instant parseDate(String orderDateAsString) {
-        try {
-            return Instant.parse(orderDateAsString);
-        } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("Invalid date format: " + orderDateAsString, e);
-        }
     }
 
     private OrderState orderStateSetToPaid(Customer customer) {
