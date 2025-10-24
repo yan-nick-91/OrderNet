94dadae5e675b4cfa4023b363ba3b93950232dd4
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-24 21:26:22 +0200

    Renamed DTO type

--- Shop-Service/src/main/java/com/yann/customerservice/application/dto/PaymentResponseDTO.java
+++ Shop-Service/src/main/java/com/yann/customerservice/application/dto/PaymentResponseDTO.java
@@ -3,6 +3,6 @@ package com.yann.customerservice.application.dto;
 public record PaymentResponseDTO(
         String orderID,
         String orderDate,
-        CustomerResponseDTO customer
+        CustomerDetailDTO customer
 ) {
 }
