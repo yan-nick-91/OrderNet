e93b85c5597800dffe4c0b78016184f66e18ab6f
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-24 21:27:46 +0200

    Removed CustomerResponseDTO

--- Orders-Service/src/main/java/com/yann/ordersservice/application/dto/PaymentResponseDTO.java
+++ Orders-Service/src/main/java/com/yann/ordersservice/application/dto/PaymentResponseDTO.java
@@ -2,7 +2,6 @@ package com.yann.ordersservice.application.dto;
 
 public record PaymentResponseDTO(
         String orderID,
-        String orderDate,
-        CustomerResponseDTO customer
+        String orderDate
 ) {
 }
