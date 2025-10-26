a99b0adf5c0c35ce6574a620c16ac3f786efedb0
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-26 18:47:20 +0100

    Extended class with IntegrationTestBase

--- Inventory-Service/src/test/java/com/yann/inventoryservice/presentation/GetProductByIDIntegrationTest.java
+++ Inventory-Service/src/test/java/com/yann/inventoryservice/presentation/GetProductByIDIntegrationTest.java
@@ -8,6 +8,7 @@ import com.yann.inventoryservice.domain.vo.ProductID;
 import com.yann.inventoryservice.domain.vo.ProductName;
 import com.yann.inventoryservice.domain.vo.ProductPrice;
 import com.yann.inventoryservice.infrastructure.repository.ProductsRepository;
+import com.yann.inventoryservice.integration.IntegrationTestBase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -28,23 +24,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 @ActiveProfiles("test")
-public class GetProductByIDIntegrationTest {
+public class GetProductByIDIntegrationTest extends IntegrationTestBase {
     private IDFactory<ProductID> productIDFactory = new ProductIDFactory();
 
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ProductsRepository productsRepository;
-
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-    }
 
     private Product savedProduct;
 
