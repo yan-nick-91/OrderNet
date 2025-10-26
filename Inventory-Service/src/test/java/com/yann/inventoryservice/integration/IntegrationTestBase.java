495c23fb0d6f4b11009f00cf46ee4c42930e74af
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-10-26 18:43:36 +0100

    init Base class for integration testing

---
+++ Inventory-Service/src/test/java/com/yann/inventoryservice/integration/IntegrationTestBase.java
@@ -0,0 +1,22 @@
+package com.yann.inventoryservice.integration;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class IntegrationTestBase {
+    static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest")
+            .withReuse(true);
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+}
+
