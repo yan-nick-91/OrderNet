316bab0049acd9d7888e3a704d3e60a29fdf4b18
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2025-11-07 01:09:59 +0100

    Init Customer-Gateway

---
+++ Customer-Gateway/src/main/java/com/yann/customergateway/CustomerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yann.customergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerGatewayApplication.class, args);
+    }
+
+}
